b2ac42c46fff3befd4129bdb73e58ceda7e0e4c3
erwinOlila <ce.olila@gmail.com>
2018-04-01 15:53:02 +0800

    added configuration variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  config: {
+    apiKey: 'AIzaSyAlvV4ZVUdPrERb4tWHfzjjj45cqFHaxr0',
+    authDomain: 'lavadoo-143.firebaseapp.com',
+    databaseURL: 'https://lavadoo-143.firebaseio.com',
+    projectId: 'lavadoo-143',
+    storageBucket: 'lavadoo-143.appspot.com',
+    messagingSenderId: '1088079662626'
+  }
 };
